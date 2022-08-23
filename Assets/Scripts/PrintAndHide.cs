8e790d36e2e697f89a5379e3a317a17f2da78e64
Vanessa Krohn <vanessa.krohn@tum.de>
2022-08-23 18:52:00 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,17 +6,29 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i = 3;
     public Renderer rend;
-    
+    private int randomVariable;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomVariable = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         i++;
-        Debug.Log(gameObject.name +":" + i);
+        
+        if (i == 100 && gameObject.CompareTag("Red"))
+        {
+            gameObject.SetActive(false);
+        }
+        
+        else if (i == randomVariable && gameObject.CompareTag("Blue"))
+        {
+            rend.enabled = false;
+        }
+        
+        Debug.Log(gameObject.name + ":" + i);
     }
 }
