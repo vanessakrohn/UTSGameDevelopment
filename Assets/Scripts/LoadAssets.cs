976719a10cfe7b9bf3ab5d352155b23925ccf5b1
Vanessa Krohn <vanessa.krohn@tum.de>
2022-08-23 17:04:17 +1000

    60 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tester : MonoBehaviour
+public class LoadAssets : MonoBehaviour
 {
+    public GameObject redObj;
+    
     // Start is called before the first frame update
     void Start()
     {
